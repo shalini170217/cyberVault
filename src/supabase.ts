2abf88985bd8031c6149bb200f0e4f2320be264a
shalini <shalinit.it2023@citchennai.net>
2025-07-08 18:25:40 +0530

    changes

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl ="https://cdfixospuyzncqiczijl.supabase.co";
-const supabaseAnonKey = import.meta.env.REACT_APP_VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
         
